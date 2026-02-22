785a009723cc15560876f2b0b72eb0d261b88f2f
Rohitkumar <sairohithbabi@gmail.com>
2026-02-22 11:10:13 +0530

    Added  binary search for first occurrence in array
    Implement binary search to find the first occurrence of a key in an array.

---
+++ Arrays/first_occurance.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,key,answer=-1;
+    cin>>n;
+    int a[n];
+   for(int i=0;i<n;i++){
+       cin>>a[i];
+   }
+  cout<<"enter key";
+  cin>>key;
+  int left=0;
+  int right=n-1;
+  while(left<=right){
+      int mid=(right+left)/2;
+      if(a[mid]==key){
+      answer=mid;
+     right=mid-1;
+  }
+  else if(key<a[mid]){
+      right=mid-1;
+  }
+  else{
+      left=mid+1;
+  }
+  }
+ 
+ cout<<answer;
+return 0;
+}
+    
+  
+  

d42a70589bcebd8560f5804de13fa324c79000bd
Rohitkumar <sairohithbabi@gmail.com>
2026-02-23 18:24:52 +0530

    added bubble sort implementation in ascending.cpp

--- Arrays/14_bubble_sort_asd.cpp
+++ Arrays/14_bubble_sort_asd.cpp
@@ -7,13 +7,12 @@ int main(){
   for(int i=0;i<n;i++){
       cin>>a[i];
   }
-  for(int i=0;i<n;i++){
-      for(int j=i+1
-      ;j<n;j++){
-          if(a[i]>a[j]){
-             int temp=a[i];
-              a[i]=a[j];
-              a[j]=temp;
+  for(int i=0;i<n-1;i++){
+      for(int j=0 ;j<n-i-1;j++){
+          if(a[j]>a[j+1]){
+             int temp=a[j];
+              a[j]=a[j+1];
+              a[j+1]=temp;
           }
       }
   }

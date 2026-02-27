5b369e681ebb918b381b73cf9c49b0ca38752cc8
Rohitkumar <sairohithbabi@gmail.com>
2026-02-27 19:03:13 +0530

    program to do reverse_an_array.cpp
    reverse an array without an extra array

---
+++ Arrays/26_reverse_an_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,s;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+  for(int i=0;i<n/2;i++){
+    int temp=a[i];
+    a[i]=a[n-1-i];
+    a[n-1-i]=temp;
+}
+for(int i=0;i<n;i++){
+    cout<<a[i];
+}
+return 0;
+    }
+    

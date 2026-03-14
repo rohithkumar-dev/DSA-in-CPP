60cd456427b6f36c85ea5c0e47496e31e9eb7250
Rohitkumar <sairohithbabi@gmail.com>
2026-03-14 21:44:18 +0530

    remove_duplicate.cpp

--- Arrays/13_remove_duplicate_sorted.cpp
+++ Arrays/13_remove_duplicate_sorted.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
+#include<algorithm>
 using namespace std;
-int main() {
+int main(){
     int n;
     cin>>n;
     int a[n];
-   for(int i=0;i<n;i++){
-       cin>>a[i];
-   }
- int pos=0;
-  for(int i=1;i<n;i++){
-      if(a[i]!=a[pos]){
-          pos++;
-          a[pos]=a[i];
-      }
-  }
-  for(int i=0;i<=pos;i++){
-      cout<<a[i];
-  }
-return 0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int k=0;
+sort(a,a+n);
+    for(int i=0;i<n-1;i++){
+        if(a[i]!=a[i+1]){
+            a[k]=a[i];
+            k++;
+        }
+    }
+    a[k]=a[n-1];
+    k++;
+    
+    for(int i=0;i<k;i++){
+        cout<<a[i];
+    }
+    return 0;
 }
     
-  
-  
-    
-  
   

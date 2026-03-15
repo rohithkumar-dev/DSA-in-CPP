039f609f96bfd3d09e0d855f7918a4ad3aa64c73
Rohitkumar <sairohithbabi@gmail.com>
2026-03-15 10:35:23 +0530

    Create 10.6_using_header_file.cpp

---
+++ Arrays/10.6_using_header_file.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+  vector<int>v;
+int n,x;
+cin>>n;
+for(int i=0;i<n;i++){
+    cin>>x;
+    v.push_back(x);
+}
+reverse(v.begin(),v.end());
+for(int i=0;i<v.size();i++){
+    cout<<v[i];
+}
+return 0;
+}

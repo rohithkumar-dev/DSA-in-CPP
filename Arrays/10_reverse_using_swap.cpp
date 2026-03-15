ba0195ffe57ffd4a3416758ecfeb766f401184a8
Rohitkumar <sairohithbabi@gmail.com>
2026-03-15 10:40:27 +0530

    Update 10_reverse_using_swap.cpp

--- Arrays/10_reverse_using_swap.cpp
+++ Arrays/10_reverse_using_swap.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
+#include<algorithm>
 using namespace std;
 int main() {
-    int n,key;
+    int n;
     cin>>n;
     int a[n];
    for(int i=0;i<n;i++){
        cin>>a[i];
    }
-  for(int i=0;i<n/2;i++){
-    int temp;
-    temp=a[0+i];
-    a[0+i]=a[n-1-i];
-    a[n-1-i]=temp;
-} 
+  //for(int i=0;i<n/2;i++){
+   // swap(a[i],a[n-1-i]);
+//}
+reverse(a,a+n);
 for(int i=0;i<n;i++){
     cout<<a[i];
 }

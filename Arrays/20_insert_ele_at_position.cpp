62884ef8cc66f79d25de9d23a58587fb0dbd4ff5
Rohitkumar <sairohithbabi@gmail.com>
2026-03-19 20:00:31 +0530

    Update 20_insert_ele_at_position.cpp

--- Arrays/20_insert_ele_at_position.cpp
+++ Arrays/20_insert_ele_at_position.cpp
@@ -1,27 +1,31 @@
-
-#include<iostream>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 using namespace std;
-int main(){
-  int n;
-  cin>>n;
-  int a[n];
-  for(int i=0;i<n;i++){
-      cin>>a[i];
-  }
- int pos,newelement;
-  cout<<"enter position";
-  cin>>pos;
-  cout<<"enter number to be inserted";
-  cin>>newelement;
-  for(int i=n-1;i>=pos-1;i--){
-      a[i+1]=a[i];
-  }
-  a[pos-1]=newelement;
-  n=n+1;
-  for(int i=0;i<n;i++){
-      cout<<a[i];
-  }
-  return 0;
+int main() {
+    int a[100];
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int newele,pos;
+    cout<<"enter newele";
+    cin>>newele;
+    cout<<"enter pos";
+    cin>>pos;
+    if(pos<0||pos>n){
+        cout<<"invalid position";
+        return 0;
+    }
+    else{
+    for(int i=n;i>=pos;i--){
+        a[i]=a[i-1];
+    }
+    }
+    a[pos]=newele;
+    n++;
+    for(int i=0;i<n;i++){
+        cout<<a[i];
+    }
+    return 0;
 }
-  
-   

9c488d3f2cb062de3b1eec441414650d9a44588b
Rohitkumar <sairohithbabi@gmail.com>
2026-03-21 11:02:53 +0530

    Update 04_last_occurance.cpp

--- Arrays/04_last_occurance.cpp
+++ Arrays/04_last_occurance.cpp
@@ -1,33 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    int n,key,answer=-1;
+int main(){
+    int a[100];
+    int n;
     cin>>n;
-    int a[n];
-   for(int i=0;i<n;i++){
-       cin>>a[i];
-   }
-  cout<<"enter key";
-  cin>>key;
-  int left=0;
-  int right=n-1;
-  while(left<=right){
-      int mid=(right+left)/2;
-      if(a[mid]==key){
-      answer=mid;
-     left=mid+1;
-  }
-  else if(key<a[mid]){
-      right=mid-1;
-  }
-  else{
-      left=mid+1;
-  }
-  }
- 
- cout<<answer;
-return 0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int value,store=-1;
+    cout<<"enter value";
+    cin>>value;
+    for(int i=0;i<n;i++){
+        if(a[i]==value){
+            store=i;
+        }
+    }
+   if(store==-1){
+    cout<<"not found";
 }
-    
-  
-  
+else{
+    cout<<"last occurence at index:"<<store;
+}
+
+    return 0;
+}

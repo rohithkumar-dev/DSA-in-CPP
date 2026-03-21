62c277fb55e2c139a4824c63029ea33b3c0c3643
Rohitkumar <sairohithbabi@gmail.com>
2026-03-21 11:06:37 +0530

    Update 05_count_occurance.cpp

--- Arrays/05_count_occurance.cpp
+++ Arrays/05_count_occurance.cpp
@@ -1,60 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int firstoccurence(int a[],int n, int key){
-    int left=0,right=n-1;
-    int first=-1;
-  while(left<=right){
-      int mid=(right+left)/2;
-      if(a[mid]==key){
-      first=mid;
-     right=mid-1;
-  }
-  else if(key<a[mid]){
-      right=mid-1;
-  }
-  else{
-      left=mid+1;
-  }
-  }
-  return first;
-}
-int lastoccurence(int a[],int n, int key){
-    int left=0,right=n-1;
-    int last=-1;
-  while(left<=right){
-      int mid=(right+left)/2;
-      if(a[mid]==key){
-      last=mid;
-     left=mid+1;
-  }
-  else if(key<a[mid]){
-      right=mid-1;
-  }
-  else{
-      left=mid+1;
-  }
-  }
-  return last; 
-}
- int main(){
-   int n,key;
+int main(){
+    int a[100];
+    int n;
     cin>>n;
-    int a[n];
-   for(int i=0;i<n;i++){
-       cin>>a[i];
-   }
-  cout<<"enter key";
-  cin>>key;
-  int first=firstoccurence(a,n,key);
-  
-  if(first==-1){
-      cout<<0;
-      return 0;
-  }
- int last=lastoccurence(a,n,key);
-  cout<<(last-first+1);
-return 0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int value,store=0;
+    cout<<"enter value";
+    cin>>value;
+    for(int i=0;i<n;i++){
+        if(a[i]==value){
+            store++;
+        }
+    }
+   if(store==0){
+    cout<<"not found";
 }
-    
-  
-  
+else{
+    cout<<"occurence frequency"<<store;
+}
+
+    return 0;
+}
